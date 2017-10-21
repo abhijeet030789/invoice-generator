90ab5609e6e33e254dfc65868046864075f4d54b
abhijeet030789 <abhijeet.rai@altisource.com>
2017-10-21 13:53:08 +0530

    Alterable Rate, Search in Transaction

--- conf/seed.sql
+++ conf/seed.sql
@@ -17,13 +17,13 @@
 --INSERT INTO Party (gstNo, name, address, state, stateCode, phoneNumber, email) VALUES('P5', 'P5-Name', 'P5-Address', 'West Bengal', '19', '7897889', 'abcd@a.com');
 --INSERT INTO Party (gstNo, name, address, state, stateCode, phoneNumber, email) VALUES('P6', 'P6-Name', 'P6-Address', 'Karnataka', '25', '7897889', 'abcd@a.com');
 
-DELETE FROM ITEM WHERE code in ('I1', 'I2', 'I3', 'I4', 'I5', 'I6');
+--DELETE FROM ITEM WHERE code in ('I1', 'I2', 'I3', 'I4', 'I5', 'I6');
 
-INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I1', 'I1-Name', 'I1-hsnCode', '30 Kg Bag', '10.0', '5.0');
-INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I2', 'I2-Name', 'I2-hsnCode', '30 Kg Bag', '20.0', '5.0');
-INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I3', 'I3-Name', 'I3-hsnCode', '30 Kg Bag', '30.0', '5.0');
-INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I4', 'I4-Name', 'I4-hsnCode', '30 Kg Bag', '40.0', '5.0');
-INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I5', 'I5-Name', 'I5-hsnCode', '30 Kg Bag', '50.0', '5.0');
-INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I6', 'I6-Name', 'I6-hsnCode', '30 Kg Bag', '60.0', '5.0');
+--INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I1', 'I1-Name', 'I1-hsnCode', '30 Kg Bag', '10.0', '5.0');
+--INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I2', 'I2-Name', 'I2-hsnCode', '30 Kg Bag', '20.0', '5.0');
+--INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I3', 'I3-Name', 'I3-hsnCode', '30 Kg Bag', '30.0', '5.0');
+--INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I4', 'I4-Name', 'I4-hsnCode', '30 Kg Bag', '40.0', '5.0');
+--INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I5', 'I5-Name', 'I5-hsnCode', '30 Kg Bag', '50.0', '5.0');
+--INSERT INTO Item (code, name, hsnCode, unit, rate, gstRate) VALUES('I6', 'I6-Name', 'I6-hsnCode', '30 Kg Bag', '60.0', '5.0');
 
-commit;
+--commit;
